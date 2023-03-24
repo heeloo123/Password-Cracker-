be946046c06e4988293fe8c18bdda9f7f1769c3d
Qian <100338414+heeloo123@users.noreply.github.com>
2023-03-24 18:21:42 +0800

    Add files via upload

---
+++ Password Cracker/generateHash.py
@@ -0,0 +1,9 @@
+import hashlib
+
+password = 'hello123'
+hash = hashlib.sha256(password.encode('utf-8')) #The utf-8 parameter specifies the specific encoding to use when converting the string to bytes.
+print(hash.hexdigest())
+
+#The use of UTF-8 encoding ensures that any characters in the password string can be properly represented as bytes and processed by the hash function.
+# UTF-8 (Unicode Transformation Format 8-bit)
+#UTF-8 represents each code point as a sequence of 1 to 4 bytes (groups of 8 bits).
